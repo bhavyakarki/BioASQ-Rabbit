56b6a4db893ae9c1af463fc7373dd163d6c0414e
bhavyakarki <bkarki@andrew.cmu.edu>
2018-03-27 14:08:47 -0400

    added old function and renamed it

--- Ranker/SimilarityJaccard.py
+++ Ranker/SimilarityJaccard.py
@@ -23,6 +23,12 @@ class SimilarityJaccard(object):
     def __init__(self):
         self.stopWords = set(stopwords.words('english'))
 
+    def calculateSimilarity_sentence(self, s1, s2):
+        set1 = set([i.lower() for i in word_tokenize(s1) if i.lower() not in self.stopWords])
+        set2 = set([i.lower() for i in word_tokenize(s2) if i.lower() not in self.stopWords])
+        return float(len(set1.intersection(set2))) / len(set1.union(set2))
+
+
     def calculateSimilarity(self, s1, s2):
         set1 = set([i.lower() for i in s1 if i.lower() not in self.stopWords])
         set2 = set([i.lower() for i in s2 if i.lower() not in self.stopWords])
